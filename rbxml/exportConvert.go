d091cf3ac91f200b3a83985a662215c1bc35a56a
Nate Randa <mail@nateranda.com>
2025-04-15 19:30:41 -0400

    add basic structure & song conversion

--- rbxml/exportConvert.go
+++ rbxml/exportConvert.go
@@ -1,7 +1,99 @@
 package rbxml
 
-import "github.com/nateranda/djtools/db"
+import (
+	"fmt"
+	"net/url"
+	"path/filepath"
+	"time"
 
-func exportConvert(library *db.Library) error {
-	return nil
+	"github.com/nateranda/djtools/db"
+)
+
+func unixToDate(date int) string {
+	t := time.Unix(int64(date), 0)
+	return t.Format("2006-01-02")
+}
+
+func pathToURI(path string) (string, error) {
+	// Convert the path to an absolute path
+	absPath, err := filepath.Abs(path)
+	if err != nil {
+		return "", fmt.Errorf("error converting path to URI: %v", err)
+	}
+	uriPath := filepath.ToSlash(absPath)
+	return "file://localhost/" + url.PathEscape(uriPath), nil
+}
+
+func exportConvertRating(rating int) (int32, error) {
+	switch rating {
+	case 0:
+		return 0, nil
+	case 20:
+		return 51, nil
+	case 40:
+		return 102, nil
+	case 60:
+		return 153, nil
+	case 80:
+		return 204, nil
+	case 100:
+		return 255, nil
+	}
+	return 0, fmt.Errorf("NoMatchError: rating %d did not match convention. Must be 0, 20, 40, 60, 80, or 100", rating)
+}
+
+func exportConvertSong(library *db.Library) ([]track, error) {
+	var tracks []track
+	for _, song := range library.Songs {
+		rating, err := exportConvertRating(song.Rating)
+		if err != nil {
+			return nil, fmt.Errorf("error converting song: %v", err)
+		}
+		path, err := pathToURI(song.Path)
+		if err != nil {
+			return nil, fmt.Errorf("error converting song: %v", err)
+		}
+		tracks = append(tracks, track{
+			trackId:      song.SongID,
+			name:         song.Title,
+			artist:       song.Artist,
+			composer:     song.Composer,
+			album:        song.Album,
+			grouping:     song.Grouping,
+			genre:        song.Genre,
+			kind:         song.Filetype,
+			size:         int64(song.Size),
+			totalTime:    float64(song.Length), // make sure this is rounded?
+			discNumber:   0,
+			trackNumber:  int32(song.TrackNumber),
+			year:         int32(song.Year),
+			averageBpm:   float64(song.Bpm),
+			dateModified: unixToDate(song.DateModified),
+			dateAdded:    unixToDate(song.DateAdded),
+			bitRate:      int32(song.Bitrate),
+			sampleRate:   song.SampleRate,
+			comments:     song.Comment,
+			playCount:    int32(song.PlayCount),
+			lastPlayed:   unixToDate(song.LastPlayed),
+			rating:       rating,
+			location:     path,
+			remixer:      song.Remixer,
+			tonality:     song.Key, // wrong key type?
+			label:        song.Label,
+			mix:          song.Mix,
+			colour:       song.Color,
+		})
+	}
+	return tracks, nil
+}
+
+func exportConvert(library *db.Library) (collection, error) {
+	var collection collection
+	var err error
+
+	collection.tracks, err = exportConvertSong(library)
+	if err != nil {
+		return collection, err
+	}
+	return collection, nil
 }
--- rbxml/rbxml.go
+++ rbxml/rbxml.go
@@ -1,9 +1,101 @@
 package rbxml
 
-import "github.com/nateranda/djtools/db"
+import (
+	"github.com/nateranda/djtools/db"
+)
+
+type product struct {
+	name    string `xml:"Name,attr"`
+	version string `xml:"Version,attr"`
+	company string `xml:"Company,attr"`
+}
+
+type track struct {
+	trackId      int     `xml:"TrackID,attr"`
+	name         string  `xml:"Name,attr"`
+	artist       string  `xml:"Artist,attr"`
+	composer     string  `xml:"Composer,attr"`
+	album        string  `xml:"Album,attr"`
+	grouping     string  `xml:"Grouping,attr"`
+	genre        string  `xml:"Genre,attr"`
+	kind         string  `xml:"Kind,attr"`
+	size         int64   `xml:"Size,attr"`
+	totalTime    float64 `xml:"TotalTime,attr"`
+	discNumber   int32   `xml:"DiscNumber,attr"`
+	trackNumber  int32   `xml:"TrackNumber, attr"`
+	year         int32   `xml:"Year,attr"`
+	averageBpm   float64 `xml:"AverageBpm,attr"`
+	dateModified string  `xml:"DateModidied,attr"` // yyyy-mm-dd
+	dateAdded    string  `xml:"DateAdded,attr"`    // yyyy-mm-dd
+	bitRate      int32   `xml:"BitRate,attr"`
+	sampleRate   float64 `xml:"SampleRate,attr"`
+	comments     string  `xml:"Comments,attr"`
+	playCount    int32   `xml:"PlayCount,attr"`
+	lastPlayed   string  `xml:"LastPlayed,attr"` // yyyy-mm-dd
+	rating       int32   `xml:"Rating,attr"`
+	location     string  `xml:"Location,attr"` // URI formatted
+	remixer      string  `xml:"Remixer,attr"`
+	tonality     string  `xml:"Tonality,attr"`
+	label        string  `xml:"Label,attr"`
+	mix          string  `xml:"Mix,attr"`
+	colour       string  `xml:"Colour,attr"` // 0x-appended hex
+}
+
+type tempo struct {
+	inizio  float64 `xml:"Inizio,attr"`
+	bpm     float64 `xml:"Bpm,attr"`
+	metro   string  `xml:"Metro,attr"` // 4/4, 3/4, etc.
+	battito int32   `xml:"Battito,attr"`
+}
+
+type positionMark struct {
+	name     string  `xml:"Name,attr"`
+	markType int32   `xml:"Type,attr"` // cue=0, fade-in=1, fade-out=2, load=3, loop=4
+	start    float64 `xml:"Start,attr"`
+	end      float64 `xml:"End,attr"`
+	num      int32   `xml:"Num,attr"` // hot cue: 0, 1, 2... memory cue: -1
+}
+
+type node struct {
+	nodeType int32       `xml:"Type,attr"` // folder=0, playlist=1
+	name     string      `xml:"Name,attr"`
+	count    int32       `xml:"Count,attr"`   // number of sub-nodes
+	entries  int32       `xml:"Entries,attr"` // number of tracks in playlist
+	keyType  int32       `xml:"KeyType,attr"` // trackId=0, location=1, should always be 0
+	tracks   []nodeTrack `xml:"TRACK"`
+	nodes    []node      `xml:"NODE"`
+}
+
+type nodeTrack struct {
+	id int32 `xml:"Key,attr"`
+}
+
+type collection struct {
+	entries int32   `xml:"Entries,attr"` // number of tracks
+	tracks  []track `xml:"TRACK"`
+}
+
+type playlists struct {
+	nodes []node `xml:"NODE"`
+}
+
+type djPlaylists struct {
+	version    string     `xml:"Version,attr"` // should always be 1,0,0
+	product    product    `xml:"PRODUCT"`
+	collection collection `xml:"COLLECTION"`
+	playlists  playlists  `xml:"PLAYLISTS"`
+}
+
+func exportInsert(collection collection, path string) error {
+	return nil
+}
 
 func Export(library *db.Library, path string) error {
-	err := exportConvert(library)
+	collection, err := exportConvert(library)
+	if err != nil {
+		return err
+	}
+	err = exportInsert(collection, path)
 	if err != nil {
 		return err
 	}
