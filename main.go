e7210c2dbc0b71eb1d509598f028241ca1e43b01
Nate Randa <mail@nateranda.com>
2025-04-24 09:41:09 -0400

    clear test code from main

--- main.go
+++ main.go
@@ -1,16 +1,3 @@
 package main
 
-import (
-	"fmt"
-	"log"
-
-	"github.com/nateranda/djtools/serato"
-)
-
-func main() {
-	crate, err := serato.ExtractCrate("tmp/Subcrates/crate 2%%crate 3.crate")
-	if err != nil {
-		log.Panic(err)
-	}
-	fmt.Printf("crate: %v\n", crate)
-}
+func main() {}
