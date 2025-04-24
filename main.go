2ed0e2c2558e3fc1c53f8568502867d223782108
Nate Randa <mail@nateranda.com>
2025-04-24 09:40:49 -0400

    refactor crate extraction

--- main.go
+++ main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+
+	"github.com/nateranda/djtools/serato"
+)
+
+func main() {
+	crate, err := serato.ExtractCrate("tmp/Subcrates/crate 2%%crate 3.crate")
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Printf("crate: %v\n", crate)
+}
--- serato/serato.go
+++ serato/serato.go
@@ -7,9 +7,83 @@ import (
 	"unicode/utf16"
 )
 
-type crateEntry struct {
-	key   string
-	value any
+type crate struct {
+	version string
+	paths   []string
+}
+
+func ExtractCrate(path string) (crate, error) {
+	var entries crate
+	var err error
+
+	file, err := os.ReadFile(path)
+	if err != nil {
+		return crate{}, err
+	}
+
+	for len(file) > 0 {
+		file, err = entries.extractCrateEntry(file)
+		if err != nil {
+			return crate{}, err
+		}
+	}
+
+	return entries, nil
+}
+
+func (c *crate) extractCrateEntry(file []byte) ([]byte, error) {
+	// extract key (4 bytes)
+	if len(file) < 4 {
+		return nil, fmt.Errorf("file too short to extract key")
+	}
+	key := string(file[:4])
+	file = file[4:]
+
+	// extract length (4 bytes)
+	length := int(binary.BigEndian.Uint32(file[:+4]))
+	file = file[4:]
+
+	// extract value based on key type (variable bytes)
+	if len(file) < length {
+		return nil, fmt.Errorf("file too short to extract value")
+	}
+	valueBytes := file[:length]
+	file = file[length:]
+
+	switch key {
+	case "otrk":
+		var err error
+		valueBytes, err = c.extractCrateEntry(valueBytes)
+		if len(valueBytes) != 0 {
+			return nil, fmt.Errorf("invalid otrk entry")
+		}
+		if err != nil {
+			return nil, err
+		}
+		return file, nil
+	case "vrsn":
+		version, err := utf16ToString(valueBytes)
+		if err != nil {
+			return nil, err
+		}
+		c.version = version
+		return file, nil
+	case "ptrk":
+		path, err := utf16ToString(valueBytes)
+		if err != nil {
+			return nil, err
+		}
+		c.paths = append(c.paths, path)
+		return file, nil
+	}
+
+	// return empty entry for unneeded key
+	switch key[:1] {
+	case "o", "t", "p", "u", "s", "b":
+		return file, nil
+	}
+
+	return nil, fmt.Errorf("key %s not of supported type", key)
 }
 
 func utf16ToString(data []byte) (string, error) {
@@ -26,77 +100,4 @@ func utf16ToString(data []byte) (string, error) {
 	runes := utf16.Decode(uint16s)
 	content := string(runes)
 	return content, nil
-}
-
-func extractCrateEntries(file []byte) ([]crateEntry, error) {
-	var crate []crateEntry
-	var err error
-	for len(file) > 0 {
-		var entry crateEntry
-		entry, file, err = extractCrateEntry(file)
-		if err != nil {
-			return nil, err
-		}
-		crate = append(crate, entry)
-	}
-	return crate, nil
-}
-
-func extractCrateEntry(file []byte) (crateEntry, []byte, error) {
-	var crate crateEntry
-
-	// extract key (4 bytes)
-	if len(file) < 4 {
-		return crateEntry{}, nil, fmt.Errorf("file too short to extract key")
-	}
-	crate.key = string(file[:4])
-	file = file[4:]
-
-	// extract length (4 bytes)
-	length := int(binary.BigEndian.Uint32(file[:+4]))
-	file = file[4:]
-
-	// extract value based on key type (variable bytes)
-	if len(file) < length {
-		return crateEntry{}, nil, fmt.Errorf("file too short to extract value")
-	}
-	valueBytes := file[:length]
-	file = file[length:]
-
-	switch crate.key {
-	case "otrk":
-		var err error
-		crate.value, err = extractCrateEntries(valueBytes)
-		if err != nil {
-			return crateEntry{}, nil, err
-		}
-		return crate, file, nil
-	case "vrsn", "ptrk":
-		value, err := utf16ToString(valueBytes)
-		if err != nil {
-			return crateEntry{}, nil, err
-		}
-		crate.value = value
-		return crate, file, nil
-	}
-
-	// return empty entry for unneeded key
-	switch crate.key[:1] {
-	case "o", "t", "p", "u", "s", "b":
-		return crateEntry{}, file, nil
-	}
-
-	return crateEntry{}, nil, fmt.Errorf("key %s not of supported type", crate.key)
-}
-
-func ExtractCrate(path string) ([]crateEntry, error) {
-	file, err := os.ReadFile(path)
-	if err != nil {
-		return nil, err
-	}
-	entries, err := extractCrateEntries(file)
-	if err != nil {
-		return nil, err
-	}
-	return entries, nil
 }
